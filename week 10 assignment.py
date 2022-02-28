c2cca481a64742fe435779215e3ce64e29c0ef9a
phildec159 <100089763+phildec159@users.noreply.github.com>
2022-02-27 19:13:41 -0800

    Add files via upload
    minor changes but still unsuccessful in getting a working program

--- week 10 assignment.py
+++ week 10 assignment.py
@@ -1,4 +1,3 @@
-import json
 import os
 import csv
 
@@ -13,20 +12,23 @@ except FileExistsError:
     print("Directory " , directory , " already exists")
     
 
-fileName = 'filename.json'
+fileName = input("What is the name of the file you would like to save?")
 try:
-    with open(fileName) as f:
-        file_name = json.load(f)
-except FileNotFoundError:
-    file_name = input("What is the name of the file you would like to save?")
     with open(fileName, 'w') as f:
-        json.dump(file_name, f)
-        print("Thank you. We have now created the file specified")
+        contents = f.read()
+except FileNotFoundError:
+    with open(fileName, 'w') as f:
+        contents = f.read()
+    print("Thank you. We have now created the file specified")
         
 print("Now we just need some information to add onto your file.")
 
 user_info= input("May we have your name, address and phone number?")
 with open(fileName, 'a') as csvfile:
-    json.dump(file_name, f)
+    f.write(user_info)
     print("Thank you. We have now added your information to your file.")
+    
+print("Your file now appears as follows:")
+f = open(fileName, 'r')
+print(f.read())
     
